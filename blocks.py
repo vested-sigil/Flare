7d0787a8b87bc005f99edefd0d505960d7e63836
vested-sigil <114979589+vested-sigil@users.noreply.github.com>
2024-01-30 01:58:17 -0600

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,27 +1,12 @@
 from msgspec import Struct
 from typing import List, Optional, Union
-class Header(Struct):
-    level: int
-    rich_text: List[RichText]
-
-class ListItem(Struct):
-    rich_text: List[RichText]
-    children: Optional[List[BlockContent]] = None  # For nested lists
-
-class ListBlock(Struct):
-    items: List[ListItem]
-
-class Quote(Struct):
-    rich_text: List[RichText]
-
-class Callout(Struct):
-    rich_text: List[RichText]
-    icon_url: Optional[str] = None
-
-class CodeBlock(Struct):
-    text: str
-    language: str
-
+class Annotations(Struct):
+    bold: bool
+    italic: bool
+    strikethrough: bool
+    underline: bool
+    code: bool
+    color: str
 class User(Struct):
     id: str
     name: Optional[str] = None
@@ -30,35 +15,46 @@ class User(Struct):
 class Text(Struct):
     content: str
     link: Optional[str] = None
-
-class Annotations(Struct):
-    bold: bool
-    italic: bool
-    strikethrough: bool
-    underline: bool
-    code: bool
-    color: str
-
 class RichText(Struct):
     type: str
     text: Text
     annotations: Annotations
     plain_text: str
     href: Optional[str] = None
-
+class Header(Struct):
+    level: int
+    rich_text: List[RichText]
+class Image(Struct):
+    type: str
+    image_url: str
+class CodeBlock(Struct):
+    text: str
+    language: str
+class Quote(Struct):
+    rich_text: List[RichText]
+class Callout(Struct):
+    rich_text: List[RichText]
+    icon_url: Optional[str] = None
 class Paragraph(Struct):
     rich_text: List[RichText]
     color: str
-
 class ToDo(Struct):
     rich_text: List[RichText]
     checked: bool
 
-class Image(Struct):
-    type: str
-    image_url: str
+BlockContent = Union[Paragraph, ToDo, Image, Header, Quote, Callout, CodeBlock]
 
-BlockContent = Union[Paragraph, ToDo, Image, Header, ListBlock, Quote, Callout, CodeBlock]
+class ListItem(Struct):
+    rich_text: List[RichText]
+    children: Optional[List[BlockContent]] = None # For nested lists
+
+class ListBlock(Struct):
+    items: List[ListItem]
+
+
+
+
+
 
 class Block(Struct):
     object: str
@@ -73,27 +69,25 @@ class Block(Struct):
     content: Optional[BlockContent] = None
 
 def extract_info(block: Block) -> dict:
-    # Extend this function to handle new block types
-    title = ""
-    if isinstance(block.content, (Paragraph, Header, Quote, Callout)):
-        title = " ".join([text.plain_text for text in block.content.rich_text])
-    elif isinstance(block.content, ToDo):
-        title = "ToDo: " + " ".join([text.plain_text for text in block.content.rich_text])
-    elif isinstance(block.content, ListBlock):
-        title = "List: " + "; ".join([" ".join([text.plain_text for text in item.rich_text]) for item in block.content.items])
-    elif isinstance(block.content, CodeBlock):
-        title = "Code: " + block.content.text[:30] + "..."  # Example: First 30 chars    if isinstance(block.content, Paragraph):
-        title = " ".join([text.plain_text for text in block.content.rich_text])
+    title_extractors = {
+        Paragraph: lambda b: " ".join([text.plain_text for text in b.content.rich_text]),
+        Header: lambda b: " ".join([text.plain_text for text in b.content.rich_text]),
+        ToDo: lambda b: "ToDo: " + " ".join([text.plain_text for text in b.content.rich_text]),
+        ListBlock: lambda b: "List: " + "; ".join([" ".join([text.plain_text for text in item.rich_text]) for item in b.content.items]),
+        CodeBlock: lambda b: "Code: " + b.content.text[:30] + "...",
+        # Add other block types here...
+    }
+    title = title_extractors.get(type(block.content), lambda b: "Unknown Block Type")(block)
     
-    info = {
+    return {
         "Title": title,
         "Block Type": block.type,
         "UUID": block.id
     }
-    return info
+
 
 def serialize_block(block: Block) -> dict:
-    return block.asdict()
+    return block.__dict__
 
 def deserialize_block(data: dict) -> Block:
     return Block(**data)
