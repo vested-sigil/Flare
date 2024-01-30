948f2edc88d5c25fa887716935a22708901008a5
vested-sigil <114979589+vested-sigil@users.noreply.github.com>
2024-01-30 03:18:20 -0600

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -1,4 +1,4 @@
-from Flare import integration
+current code is(from Flare import integration
 from Flare.integration import Integration, portal
 
 root_page = portal.get_entity("pages", portal.rootuuid)
@@ -25,23 +25,27 @@ def UnitTestTwo():
         }
     }
     testblock = {'id': None}
-    response = portal.append_block_children(portal.rootuuid, [test_block])
+    
+    for _ in range(3):
+        # Append the test block to the root page
+        response = portal.append_block_children(portal.rootuuid, [test_block])
 
-    # Assert that the block was added successfully
-    assert response is not None
-    assert response["object"] == "list"
-    assert len(response["results"]) > 0
-    assert response["results"][0]["object"] == "block"
+        # Assert that the block was added successfully
+        assert response is not None
+        assert response["object"] == "list"
+        assert len(response["results"]) > 0
+        assert response["results"][0]["object"] == "block"
 
-    # Store the UUID of the appended block
-    if response["results"][0]["id"] == portal.rootuuid:
-        # If the appended block is the root block, find the UUID of the last child
-        children = portal.get_entity("block", portal.rootuuid, recursive=True)['children']
-        assert len(children) > 0
-        testblock['id'] = children[-1]["id"]
-    else:
-        testblock['id'] = response["results"][0]["id"]
+        # Store the UUID of the appended block
+        if response["results"][0]["id"] == portal.rootuuid:
+            # If the appended block is the root block, find the UUID of the last child
+            children = portal.get_entity("block", portal.rootuuid, recursive=True)['children']
+            assert len(children) > 0
+            testblock['id'] = children[-1]["id"]
+        else:
+            testblock['id'] = response["results"][0]["id"]
 
-    # Clean up: Remove the test block if it's not the root block
-    if testblock['id'] != portal.rootuuid:
-        portal.remove_block(testblock['id'], response["results"][0]["id"])
+        # Clean up: Remove the test block if it's not the root block
+        if testblock['id'] != portal.rootuuid:
+            portal.remove_block(testblock['id'])
+
