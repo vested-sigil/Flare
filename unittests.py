3ca97f32967b4e77dcbc82f81a71d7ea6c647074
vested-sigil <114979589+vested-sigil@users.noreply.github.com>
2024-01-30 03:08:06 -0600

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -24,23 +24,24 @@ def UnitTestTwo():
             ]
         }
     }
+    testblock = {'id': None}
     response = portal.append_block_children(portal.rootuuid, [test_block])
 
-# Assert that the block was added successfully
+    # Assert that the block was added successfully
     assert response is not None
     assert response["object"] == "list"
     assert len(response["results"]) > 0
     assert response["results"][0]["object"] == "block"
 
-# Store the UUID of the appended block
+    # Store the UUID of the appended block
     if response["results"][0]["id"] == portal.rootuuid:
-    # If the appended block is the root block, find the UUID of the last child
+        # If the appended block is the root block, find the UUID of the last child
         children = portal.get_entity("block", portal.rootuuid, recursive=True)['children']
         assert len(children) > 0
-        testblock.id = children[-1]["id"]
+        testblock['id'] = children[-1]["id"]
     else:
-        testblock.id = response["results"][0]["id"]
+        testblock['id'] = response["results"][0]["id"]
 
-# Clean up: Remove the test block if it's not the root block
-    if testblock.id != portal.rootuuid:
-        portal.remove_block(testblock.id, response["results"][0]["id"])
+    # Clean up: Remove the test block if it's not the root block
+    if testblock['id'] != portal.rootuuid:
+        portal.remove_block(testblock['id'], response["results"][0]["id"])
